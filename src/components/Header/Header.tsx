e34365864deb079308a179130913752303338773
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-13 22:10:35 -0300

    feat: started to implemente the header of the application and used clerk to handle the sign up and sign in buttons

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, SignUp, SignUpButton, UserButton, UserProfile, useUser } from '@clerk/nextjs'
+import { currentUser, User } from '@clerk/nextjs/server'
+import Link from 'next/link'
+
+export default async function Header (){
+
+    const user = await currentUser()
+    const username = user?.firstName
+    return (
+        <>
+            <nav className='flex flex-row items-center justify-between'>
+                <Link href='/'><button type="button" className="p-2">Home Page</button></Link>
+                <input type="text" className="solid border-black border-2" placeholder="Search right here"/>
+                <button type="button" className="p-2">New Product</button>
+                <button type="button" className='p-2'>About</button>
+                <SignedOut>
+                    <SignInButton><button className='p-2'>Sign In!</button></SignInButton>
+                    <SignUpButton><button className='p-2'>Register!</button></SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                    <h2 className='p-2'>Welcome, {username}!</h2>
+                    <UserButton></UserButton>
+                </SignedIn>
+            </nav>
+        </>
+    )
+}

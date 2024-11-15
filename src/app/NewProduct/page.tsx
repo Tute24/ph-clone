9ace2b6b0d1609dabfa0be891d96caa1d62c6391
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-15 19:02:53 -0300

    feat: brought the handleInputChange and handleSubmit to the NewProduct page for modularization best practices

--- src/app/NewProduct/page.tsx
+++ src/app/NewProduct/page.tsx
@@ -1,12 +1,41 @@
+'use client'
+
 import ProductForm from "@/components/ProductForm/ProductForm";
+import { useContextWrap } from "@/contexts/ContextWrap";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 
 
-export default async function NewProduct(){
+export default function NewProduct(){
+
+    const {productInfos,setProductInfos} = useContextWrap()
+
+    function handleInputChange(e:any){
+        setProductInfos(
+            {
+                ...productInfos,
+            [e.target.name] : e.target.value
+            }
+        )
+    }
+
+    async function handleSubmit(e:any){
+        e.preventDefault()
+        
+        try{
+            
+        } catch(error){
+            console.log(error)
+        }
+    }
+    
     return(
         <>
             <SignedIn>
-                <ProductForm/>
+                <ProductForm 
+                    productInfos={productInfos}
+                    onInputChange={handleInputChange}
+                    onSubmit={handleSubmit}
+                />
             </SignedIn>
             <SignedOut>
                 <h2>
--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,46 +1,47 @@
 "use client"
-import { useContextWrap } from "@/contexts/ContextWrap"
+interface ProductFormsProps{
+    productInfos:{
+            productName: string,
+            description: string,
+            productUrl: string,
+            tags:string[]
+    },
+    onInputChange: (e:any)=> void
+    onSubmit: (e:any) => void
+}
 
-export default function ProductForm(){
+export default function ProductForm({
+        productInfos,
+        onInputChange,
+        onSubmit
+    }: 
+        ProductFormsProps
+    )
 
-    const {productInfos, setProductInfos} = useContextWrap()
-
-    function handleInputChange(e:any){
-        setProductInfos(
-            {
-                ...productInfos,
-            [e.target.name] : e.target.value
-            }
-        )
-    }
-
-    function handleSubmit(e:any){
-        e.preventDefault()
-        console.log(productInfos)
-    }
-
+    {
+    
     return (
         <div >
                 <h2>
                     Add a new product to the site's products list:
                 </h2>
-                <form className="flex flex-col w-2/5" onSubmit={handleSubmit} >
+                <form className="flex flex-col w-2/5" onSubmit={onSubmit} >
                     <label htmlFor="productName">
                         Product name:
                     </label>
-                    <input className="solid border-black border-2" id="productName" type="text" name="productName" value={productInfos.productName} onChange={handleInputChange}/>
+                    <input className="solid border-black border-2" id="productName" type="text" name="productName" value={productInfos.productName} onChange={onInputChange}/>
                     <label htmlFor="description">
                         Product description:
                     </label>
-                    <input className="solid border-black border-2" id="description" type="text" name="description" value={productInfos.description} onChange={handleInputChange}/>
+                    <input className="solid border-black border-2" id="description" type="text" name="description" value={productInfos.description} onChange={onInputChange}/>
                     <label htmlFor="productUrl">
                         The URL to your product's webpage:
                     </label>
-                    <input className="solid border-black border-2" id="productUrl" type="text" name="productUrl" value={productInfos.productUrl} onChange={handleInputChange}/>
+                    <input className="solid border-black border-2" id="productUrl" type="text" name="productUrl" value={productInfos.productUrl} onChange={onInputChange}/>
                     <label htmlFor="tags">
                         Your product's tags/categories
                     </label>
-                    <input className="solid border-black border-2 mb-1" id="tags" type="text" name="tags" value={productInfos.tags} onChange={handleInputChange}/>
+                    <input className="solid border-black border-2 mb-1" id="tags" type="text" name="tags" value={productInfos.tags} onChange={onInputChange}/>
                     <button className="bg-orange-400 " type="submit">
                         Add new product
                     </button>

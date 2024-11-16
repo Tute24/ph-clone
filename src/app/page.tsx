68c41823219324aaee9d5e5ebc81d9fc940240a7
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-16 17:00:59 -0300

    feat: implemented the get request for fetching the products list on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function HomePage(){
+import axios from "axios"
+
+export default async function HomePage(){
+
+    try{
+        const response = await axios.get('/productsList')
+    }catch(error){
+
+    }
+
     return(
-        <h1>Hi.</h1>
+        <h1 className="flex justify-center p-4 font-bold">Products:</h1>
     )
 }

0613c6ef9628d2a98a3f431c822ca67e84c8b311
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-18 00:04:13 -0300

    feat: added the tags do the products li

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react"
 export default function HomePage(){
 
     const [productsArray, setProductsArray] = useState([])
+    const [tagsArray, setTagsArray] = useState<string[]>([])
 
     useEffect(()=>{
         async function fetchProducts(){
@@ -14,8 +15,22 @@ export default function HomePage(){
                 const response = await axios.get('http://localhost:3000/productsList')
                 if(response){
                     const productsData = response.data.products
+                    const tagsFetch: string[] = productsData.flatMap((e:{
+                        _id: string,
+                        description:string,
+                        productName:string,
+                        productUrl: string,
+                        tags:string[],
+                        upVotes: number
+                    } ) => e.tags.flatMap(tag => tag.split(/[,]+/)))
+                    console.log(tagsFetch)
                     console.log(productsData)
                     setProductsArray(productsData)
+                    setTagsArray([... tagsArray])
+
+                    const uniqueTags = [... new Set (tagsFetch)]
+                    setTagsArray(uniqueTags)
+                    
                 }
             }catch(error){
                 console.log(error)
@@ -39,10 +54,18 @@ export default function HomePage(){
                     upVotes: number
                 }) => (
                         <div key={e._id} className="p-5 border-gray-400 w-3/5">
-                            <li className="flex flex-col justify-center border-solid border-2 shadow-md rounded-lg hover:shadow-lg" key={e._id}>
+                            <li key={e._id} className="flex flex-col justify-center border-solid border-2 shadow-md rounded-lg hover:shadow-lg" >
                                 <h2 className="font-bold p-2">{e.productName}</h2>
-                                <p >About: {e.description}</p>
-                                <Link href={e.productUrl} target="blank" className="mt-2 underline text-orange-500 ">Official Website</Link>
+                                <div className="flex flex-row justify-evenly items-center p-2">
+                                    <p >About: {e.description}</p>
+                                    <Link href={e.productUrl} target="blank" className=" underline text-orange-500 ">Official Website</Link>
+                                </div >
+                                <div className="text-xs p-2 flex flex-row gap-2">
+
+                                    {e.tags.flatMap(tag => tag.split(/[,]+/)).map((item, index) =>(
+                                             <span className="underline" key={`${e._id}-${index}`}>{item}</span>
+                                        ))}
+                                </div>
                             </li>
                         </div>
                         )

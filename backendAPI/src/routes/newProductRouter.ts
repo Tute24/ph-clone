240d4a65410a9b70294940d851ae4f7101e37025
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-22 17:59:24 -0300

    feat: implemented the summarized Description property, featured by chatgpt API

--- backendAPI/src/routes/newProductRouter.ts
+++ backendAPI/src/routes/newProductRouter.ts
@@ -1,16 +1,33 @@
 import express, { Request, Response } from 'express'
 const newProductRouter = express.Router()
 import Product from '../Schemas/product'
+import OpenAI from 'openai'
 
 
 
 newProductRouter.post('/newProduct', async (req: Request, res: Response): Promise<any> =>{
+        const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
         const {productName, description, productUrl,tags } = req.body
 
         try{
+
+            const response = await openai.chat.completions.create({
+                model: 'gpt-4o-mini',
+                messages: [
+                    {role: 'system', content: 'Chat, you must summarize the passed text into a 10-word max description about it, please.'},
+                    {role: 'user', content: description}
+                ],
+                stream: false
+            })
+
+            const summDesc = response.choices[0].message.content
+            
+            console.log(summDesc)
+
             const newProduct = new Product({
                 productName,
                 description,
+                summDesc,
                 productUrl,
                 tags
             })

7538026049728c7d6ab33393e69b5570d8fd842a
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-30 17:28:16 -0300

    fix: trying to make the usercreatedroute to work

--- backendAPI/src/routes/userCreatedRouter.ts
+++ backendAPI/src/routes/userCreatedRouter.ts
@@ -3,14 +3,8 @@ const userCreatedRouter: Router = express.Router()
 import User from "../Schemas/user";
 
 userCreatedRouter.post('/clerkusercreated', async (req:Request,res:Response)=>{
-    const {first_name,lastName,username,emailAddress} = req.body
-
-   
-    try{
-        console.log(first_name)
-    }catch(error){
-        console.log(error)
-    }
+   console.log(req.body)
+   res.status(200).send('Webhook Recebido!')
 })
 
 export default userCreatedRouter

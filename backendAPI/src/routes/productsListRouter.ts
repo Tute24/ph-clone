07014ed4f1757c10b498e3bf71529ddefc7698ce
Arthur Araujo <arthurtute200@hotmail.com>
2024-12-02 19:39:21 -0300

    fix: trying the track an error on the productsList route

--- backendAPI/src/routes/productsListRouter.ts
+++ backendAPI/src/routes/productsListRouter.ts
@@ -3,15 +3,19 @@ const productsListRouter = express.Router()
 import Product from '../Schemas/product'
 
 productsListRouter.get('/productsList', async (req: Request,res:Response):Promise<any>=>{
-
+    console.log('Teste1')
     try{
+        console.log('Teste 2')
         const productsList: string[] = await Product.find({}, 'productName description productUrl upVotes tags summDesc')
+        console.log('Teste 3')
         if(productsList){
+            console.log('Teste 4')
             return res.json({message:'Fetched successfully.', products: productsList})
+           
         }
 
     }catch(error){
-        return res.status(500).json({message: 'Error'})
+        return res.status(500).json({message: 'Error desgraça'})
     }
 })
 

702918dcaa376ea19fefb533335b456539198c46
Arthur Araujo <arthurtute200@hotmail.com>
2024-12-01 22:50:21 -0300

    feat: implemented the route request to get the logged user infos on frontend, using clerk-js to access his token

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,48 @@
+'use client'
+
+import { Clerk } from "@clerk/clerk-js"
+import { SignedIn, SignedOut } from "@clerk/clerk-react"
+import axios from "axios"
+
+import { useEffect } from "react"
+
+
 export default function Dashboard(){
+    
+    useEffect(()=>{
+        async function getDashboard() {
+            const nextPublicKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+            if(!nextPublicKey){
+                throw new Error('Publick Key not provided!')
+            }
+
+            const clerk = new Clerk(nextPublicKey)
+            try{
+                await clerk.load()
+                const token = await clerk.session?.getToken()
+                if(!token){
+                    console.log('No token provided!')
+                    return
+                }
+                console.log(token)
+                const response = await axios.get('/dashboard',{headers:{
+                    'Authorization': `Bearer ${token}`
+                }})
+            }catch(error){
+                console.log(error)
+            }
+            
+    }
+    getDashboard()
+    },[])
     return(
-        <h2>Oi Mundo</h2>
+        <>
+        <SignedOut>
+            <h2>Você não está logado!</h2>
+        </SignedOut>
+        <SignedIn>
+            <h2>Oi Mundo</h2>
+        </SignedIn>
+        </>
     )
 }

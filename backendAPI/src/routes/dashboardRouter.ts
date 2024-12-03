13c72d035081cdd4938a2bbc0f0a38731ca1da01
Arthur Araujo <arthurtute200@hotmail.com>
2024-12-02 21:22:50 -0300

    fix: the page was loggin the user out because of an error on clerk.load(). Now its fixed using useSession

--- backendAPI/src/routes/dashboardRouter.ts
+++ backendAPI/src/routes/dashboardRouter.ts
@@ -1,16 +1,20 @@
-import express, { Router, Request,Response } from 'express'
+import express, { Router, Request, Response } from 'express'
 const dashRouter: Router = express.Router()
-import Middleware from '../middleware/AuthMiddleware'
+
 import { getAuth } from '@clerk/express'
 
-dashRouter.get('/dashboard', async (req:Request & {user?:any},res:Response): Promise<any> =>{
-    console.log("Iniciando requisição")
-    try{const user = getAuth(req)
-    console.log(user)
-    res.status(200).json({message:"Success!",user: user})
-    }catch(error){
-        res.status(500).json({message:"Server Error"})
+dashRouter.get(
+  '/dashboard',
+  async (req: Request & { user?: any }, res: Response): Promise<any> => {
+    console.log('Iniciando requisição')
+    try {
+      const user = getAuth(req)
+      console.log(user)
+      res.status(200).json({ message: 'Success!', user: user })
+    } catch (error) {
+      res.status(500).json({ message: 'Server Error' })
     }
-})
+  }
+)
 
 export default dashRouter
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,51 @@
 'use client'
 
-import { Clerk } from "@clerk/clerk-js"
-import { SignedIn, SignedOut } from "@clerk/clerk-react"
-import axios from "axios"
+import { Clerk } from '@clerk/clerk-js'
+import { SignedIn, SignedOut, useClerk, useSession } from '@clerk/clerk-react'
+import axios from 'axios'
 
-import { useEffect } from "react"
+import { useEffect, useState } from 'react'
 
-export default function Dashboard(){
-    
-    useEffect(()=>{
-        async function getDashboard() {
-            
-            try{
-                
-                const response = await axios.get('https://ph-clone.onrender.com/dashboard')
-            }catch(error){
-                console.log(error)
-            }
-            
+export default function Dashboard() {
+  const { session } = useSession()
+  const [isSeassionLoaded, setIsSessionLoaded] = useState<boolean>(false)
+
+  
+
+  useEffect(() => {
+    async function getDashboard() {
+        if(session){
+            setIsSessionLoaded(true)
+        }
+        
+      
+
+      try {
+        if(isSeassionLoaded){
+        const token = await session?.getToken()
+        const response = await axios.get(
+          'https://ph-clone.onrender.com/dashboard',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        console.log(response.data)}
+      } catch (error) {
+        console.log(error)
+      }
     }
     getDashboard()
-    },[])
-    return(
-        <>
-        <SignedOut>
-            <h2>Você não está logado!</h2>
-        </SignedOut>
-        <SignedIn>
+  }, [session,isSeassionLoaded])
+  return (
+    <>
+      <SignedOut>
+        <h2>Você não está logado!</h2>
+      </SignedOut>
+      <SignedIn>
         <h2>Oi Mundo</h2>
-        </SignedIn>
-        </>
-    )
+      </SignedIn>
+    </>
+  )
 }

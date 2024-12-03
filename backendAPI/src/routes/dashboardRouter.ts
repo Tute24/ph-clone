5cc594f877de9167a2172a642a8c4d00dc0c8389
Arthur Araujo <arthurtute200@hotmail.com>
2024-12-03 20:03:37 -0300

    feat: created the logic to the dashboardRouter to get the user's products for the dashboard page

--- backendAPI/src/routes/dashboardRouter.ts
+++ backendAPI/src/routes/dashboardRouter.ts
@@ -1,16 +1,16 @@
 import express, { Router, Request, Response } from 'express'
 const dashRouter: Router = express.Router()
-
+import Product from '../Schemas/product'
 import { getAuth } from '@clerk/express'
 
 dashRouter.get(
   '/dashboard',
   async (req: Request & { user?: any }, res: Response): Promise<any> => {
     console.log('Iniciando requisição')
+    const userId = getAuth(req).userId
     try {
-      const user = getAuth(req)
-      console.log(user)
-      res.status(200).json({ message: 'Success!', user: user })
+      const usersProducts = await Product.find({createdBy: userId}, "productName description productUrl upVotes tags summDesc")
+      res.status(200).json({ message: 'Success!',products: usersProducts})
     } catch (error) {
       res.status(500).json({ message: 'Server Error' })
     }

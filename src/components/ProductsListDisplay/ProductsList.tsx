8d506e33e005e8b92dc8e174e4b3d5c814ba512e
Arthur Araujo <arthurtute200@hotmail.com>
2024-12-10 21:02:34 -0300

    feat: implemented the voterCheckHandler on the tags page, homepage and the dashboard

--- src/components/ProductsListDisplay/ProductsList.tsx
+++ src/components/ProductsListDisplay/ProductsList.tsx
@@ -11,7 +11,7 @@ interface ListProps {
   setRankingIndex: (value: number) => void
   setUpVoteProduct: (value: { product: string }) => void
   displayUpVote: (value: string) => void
-  voterCheckHandle: (value:string) => void
+  voterCheckHandler: (value:string) => void
 }
 
 export default function ProductsList({
@@ -21,7 +21,7 @@ export default function ProductsList({
   setRankingIndex,
   setUpVoteProduct,
   displayUpVote,
-  voterCheckHandle
+  voterCheckHandler
 }: ListProps) {
   
   return (
@@ -86,7 +86,7 @@ export default function ProductsList({
                         product: product._id,
                       })
                       displayUpVote(product._id)
-                      voterCheckHandle(product._id)
+                      voterCheckHandler(product._id)
                     }}
                     className="px-2 border-solid border-2 border-gray-200 rounded-md"
                     type="button"
